09d9dd19a7d6a6d4452e277fbd18889b5536f2bc
Veselina Kolova <vmkolova@gmail.com>
2016-12-20 12:19:32 +0200

    Snake Game Changes

--- Snake-Game/app.js
+++ Snake-Game/app.js
@@ -1,6 +1,10 @@
 (function() {
 
-  var Board = function() {
+  var Board = function(width, heigh) {
+    boardW = width
+    boardH = heigh
+    squareW = boardW / 40
+    squareH = boardH / 40
 
     var canvas = document.querySelector('#render-target')
     var snake = new PointsList()
@@ -13,7 +17,7 @@
 
     function clearBoard() {
       var ctx = canvas.getContext("2d")
-      ctx.clearRect(0, 0, 600, 600)
+      ctx.clearRect(0, 0, boardW, boardH)
     }
 
     function PointsList() {
@@ -80,8 +84,8 @@
     }
 
     function getRandomPosition() {
-      return { x: Math.round(Math.floor(1 + Math.random() * (600 - 1)) / 15) * 15,
-               y: Math.round(Math.floor(1 + Math.random() * (600 - 1)) / 15) * 15}
+      return { x: Math.round(Math.floor(1 + Math.random() * (boardW - 1)) / squareW) * squareW,
+               y: Math.round(Math.floor(1 + Math.random() * (boardH - 1)) / squareH) * squareH}
     }
 
     function render() {
@@ -89,12 +93,12 @@
 
       ctx.fillStyle = "rgb(41, 41, 41)"
       snake.list.forEach(function(p) {
-        ctx.fillRect(p.x, p.y, 15, 15)
+        ctx.fillRect(p.x, p.y, squareW, squareH)
       })
 
-      ctx.fillStyle = "rgb(245, 37, 0)"
       apples.list.forEach(function(p) {
-        ctx.fillRect(p.x, p.y, 15, 15)
+        ctx.fillStyle = p.color
+        ctx.fillRect(p.x, p.y, squareW, squareH)
       })
     }
 
@@ -105,6 +109,13 @@
     function gameOver() {
       clearInterval(gameLoop)
       $game.innerHTML = "GAME OVER"
+    }
+
+    function handleScore(sc) {
+      score += sc || 0
+      if (score < 0) console.log("!!!")
+      $score.innerHTML = score
+
     }
 
     return {
@@ -126,17 +137,18 @@
       getApple: getApple,
       gameOver: gameOver,
       $score: $score,
-      $game: $game
+      $game: $game,
+      handleScore: handleScore
     }
   }
 
-  var Snake = function(board) {
-    var length = 5
+  var Snake = function(board, length) {
+    var length = length || 5
     var dir = "right"
 
     //Init snake
     for (var i = length - 1; i >= 0; i--) {
-      board.addPoint( {x: i*15, y: 0} )
+      board.addPoint( {x: i*squareW, y: 0} )
     }
 
     function motion() {
@@ -144,64 +156,55 @@
       var head = board.getSnakeHead()
 
       //Get the directions
-      document.onkeydown = function(e) {
-        var key = e.keyCode
-
-        if(key == 37 && dir != "right") setTimeout(function() {dir = "left" }, 30)
-        else if(key == 38 && dir != "down") setTimeout(function() {dir = "up" }, 30)
-        else if(key == 39 && dir != "left") setTimeout(function() {dir = "right" }, 30)
-        else if(key == 40 && dir != "up") setTimeout(function() {dir = "down" }, 30)
-
-        if(key) e.preventDefault()
-      }
-
-      if (dir == "right") head.x += 15
-      else if (dir == "left") head.x -= 15
-      else if (dir == "up") head.y -= 15
-      else if (dir == "down") head.y += 15
-
+      document.onkeydown = handleDirection
+      move(head, dir)
       //Move snake
       board.popPoint()
       board.unshiftPoint(head)
 
       //Wall Collision
-      if ( head.x < 0 || head.x > 600 || head.y > 600 || head.y < 0 ) {
+      if ( head.x < 0 || head.x > boardW || head.y > boardH || head.y < 0 ) {
         board.gameOver()
       }
 
-      //Apple collision
-      if (board.indexOfApple(head) > -1) {
-        var tail = head
-        var apple = board.getApple(head)
-        if (apple.e == 0) {
-          length++
-          board.unshiftPoint(tail)
-                  }
-        else if (apple.e == 1) {
-          length--
-          board.popPoint()
-          if (length == 0) board.gameOver()
-        } else if (apple.e == 2 && board.timeout <= 45) {
-          board.timeout++
-        } else if (apple.e == 3 && board.timeout > 1) {
-          board.timeout--
-        }
+      apple.collide(head)
 
-        board.score += 10
-        board.$score.innerHTML = board.score
-        board.removeApple(head)
-      } else if (board.indexOfPoint(head) > -1) {
-        board.gameOver()
-      }
+    }
+
+    function handleDirection(e) {
+      var key = e.keyCode
+
+      if(key == 37 && dir != "right") setTimeout(function() {dir = "left" }, 30)
+      else if(key == 38 && dir != "down") setTimeout(function() {dir = "up" }, 30)
+      else if(key == 39 && dir != "left") setTimeout(function() {dir = "right" }, 30)
+      else if(key == 40 && dir != "up") setTimeout(function() {dir = "down" }, 30)
+
+      if(key) e.preventDefault()
+    }
+
+    function move(head, dir) {
+      if (dir == "right") head.x += squareW
+      else if (dir == "left") head.x -= squareW
+      else if (dir == "up") head.y -= squareH
+      else if (dir == "down") head.y += squareH
     }
 
     return {
-      move: motion
+        move: motion,
+        length: length,
       }
   }
 
   var Apple = function(board) {
     var position
+
+    var apples = [
+      { score: 5, color: "#F71850" , length: 1, timeout: 1 },
+      { score: 10, color: "#22F253", length: 1, timeout: 1 },
+      { score: 15, color: "#F0D524", length: 1, timeout: 3 },
+      { score: 0, color: "#474747", length: -1, timeout: -1 },
+      { score: -15, color: "#F0AC24" , length: -1, timeout: 1 },
+    ]
 
     function newApple() {
       var pos = board.random()
@@ -209,25 +212,55 @@
         pos = board.random()
       }
 
-      var energy = Math.floor(Math.random() * 3)
-      board.addApple( {x: pos.x, y: pos.y, e: energy} )
+      var apple = apples[Math.floor(Math.random() * apples.length)]
+      apple.x = pos.x
+      apple.y = pos.y
+      board.addApple( apple )
 
-      if (energy === 1 && !board.moreApples()) newApple()
     }
+
+    function collide(head) {
+      if (board.indexOfApple(head) > -1) {
+        var tail = head
+        var apple = board.getApple(head)
+
+        board.handleScore(apple.score)
+
+        snake.length += apple.length
+        if (snake.length == 0) board.gameOver()
+
+        if (apple.length == -1)
+          board.popPoint()
+        else
+          board.unshiftPoint(tail)
+
+        board.timeout += apple.timeout
+
+        board.handleScore()
+        board.removeApple(head)
+
+      } else if (board.indexOfPoint(head) > -1 ) {
+        board.gameOver()
+      }
+    }
+
     return {
-      new: newApple
+      new: newApple,
+      collide: collide
     }
   }
 
   var Game = (function() {
+
     function start() {
-        var board = new Board()
-        board.$score.innerHTML = board.score
-        var snake = new Snake(board)
-        var apple = new Apple(board)
+        var board = new Board(600, 600)
+        board.handleScore()
+
+        apple = new Apple(board)
+        snake = new Snake(board)
         apple.new()
 
-        gameLoop = setInterval(game, 1000/board.timeout)
+        gameLoop = setInterval(game, 1000 / board.timeout)
 
         function game() {
           board.clear()
@@ -236,7 +269,7 @@
 
           if (board.moreApples()) {
             clearInterval(gameLoop)
-            gameLoop = setInterval(game, 1000/board.timeout)
+            gameLoop = setInterval(game, 1000 / board.timeout)
             apple.new()
           }
         }
