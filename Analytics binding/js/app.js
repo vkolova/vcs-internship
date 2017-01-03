f5a61a02daa7a2683ac572b35140bb8e91bd6fe5
Veselina Kolova <vmkolova@gmail.com>
2017-01-03 12:42:14 +0200

    Analytics binding

--- Analytics binding/js/app.js
+++ Analytics binding/js/app.js
@@ -1,31 +1,31 @@
 ko.bindingHandlers.analytics = {
   init: (element, valueAccessor, allBindings) => {
-  },
+    jQuery( ".send-info" ).on( "click mouseover", (evn) => {
 
-  update: (element, valueAccessor, allBindings) => {
+      var value = valueAccessor()
+      var gaObj = (typeof value === 'object' && !(value instanceof Array))
 
-    var value = valueAccessor()
-    console.log(value)
+      if (gaObj) {
 
-    var gaObj = (typeof value === 'object' && !(value instanceof Array))
-
-    if (gaObj) {
-
-      var data = {
-        hitType: 'event',
-        eventCategory:
+        var data = {
+          hitType: evn.type,
+          eventCategory:
           value.category != undefined ? value.category : 'USER_ACTION_PORTAL',
-        eventAction: value.action,
-        eventLabel: value.label != undefined ? value.label : '',
-        eventValue: value.value != undefined ? 1 : 0
-      }
-      // ga('send', data)
-      console.log("ga('send', ", data, ")")
+          eventAction: value.action,
+          eventLabel: value.label != undefined ? value.label : ''
+        }
 
-    }
+        if (value.value === parseInt(value.value, 10)) data.eventValue = parseInt(value.value, 10)
+        else if (value.value === true || value.value === 'true') data.eventValue = 1
+        else if (value.value === false || value.value === 'false') data.eventValue = 0
+
+        // ga('send', data)
+        console.log("ga('send', ", data, ")")
+      }
+
+    })
   }
 }
-
 
 AppViewModel = () => {
   var self = this
