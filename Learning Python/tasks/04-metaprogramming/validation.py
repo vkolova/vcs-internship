6bb3ae7b42e37f01738fdead339eb31053f2a2b0
Yavor Atov <yavor.atov@gmail.com>
2017-01-06 13:07:44 +0200

    Added some Python tasks

---
+++ Learning Python/tasks/04-metaprogramming/validation.py
@@ -0,0 +1,30 @@
+import numbers
+
+"""
+@validate(
+    arg('x', [number, lambda x: 0 < x]),
+    arg('y', [instance(Iterable)]),
+)
+def calc(x, y):
+    pass
+"""
+
+
+def is_number(n):
+    pass
+
+
+def is_string(n):
+    pass
+
+
+def is_instanceof(type):
+    pass
+
+
+def arg(name, validators):
+    pass
+
+
+def validate(*validators):
+    pass
