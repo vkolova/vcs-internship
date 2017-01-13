47eb5961d4af892a33fbd9d0e9368c730fadd6cb
Veselina Kolova <vmkolova@gmail.com>
2017-01-13 14:52:08 +0200

    django tutorial done

--- Learning Python/mysite/polls/views.py
+++ Learning Python/mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
-fron django.utils import timezone
+from django.utils import timezone
 
 # Create your views here.
 from .models import Choice, Question
@@ -17,7 +17,7 @@ class IndexView(generic.ListView):
         published in the future).
         """
         return Question.objects.filter(
-            pub_date_lte=timezone.now()
+            pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
 
@@ -25,9 +25,21 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now(), )
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
