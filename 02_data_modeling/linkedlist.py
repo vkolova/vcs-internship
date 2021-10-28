f9a79505ab8227597dd5ca0e482e0111080dfdbc
Veselina Kolova <vmkolova@gmail.com>
2021-10-28 16:55:04 +0000

    Add Data Modeling ✨

---
+++ 02_data_modeling/linkedlist.py
@@ -0,0 +1,45 @@
+class Cons:
+    pass
+
+
+nil = "?"
+
+
+def make_list(items):
+    pass
+
+
+"""
+Create a linked list with idiomatic python interface
+
+Creation
+========
+
+    >>> l = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, nil)))))
+    >>> l
+    <1, 2, 3, 4, 5>
+    >>> l = make_list(1, 2, 3, 4, 5)
+    >>> l
+    <1, 2, 3, 4, 5>
+
+Iteration
+=========
+
+    >>> [li + 1 for li in l]
+    [2, 3, 4, 5, 6]
+
+Indexing:
+=========
+
+    >>> l[1]
+    2
+    >>> l[-2]
+    4
+
+"""
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
