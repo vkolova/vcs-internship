795cf7024bc48f5b485ce4fa70262912e44466cc
stefangrgv <stefan.grgv@gmail.com>
2022-06-02 10:24:26 +0300

    changed return type

--- 01_lists/getting-started-2.py
+++ 01_lists/getting-started-2.py
@@ -1,6 +1,6 @@
 def test_palindromes(a, b):
     reversed_a = a[::-1]
     if reversed_a.lower() == b.lower():
-        return 'PALINDROMES'
+        return True
     else:
-        return 'NOT PALINDROMES'
+        return False
