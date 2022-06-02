8fae9db8473deef6c868bf3630a61d4a1d1755ce
stefangrgv <stefan.grgv@gmail.com>
2022-06-02 14:25:16 +0300

    changed exception type

--- 01_lists/getting-started-4.py
+++ 01_lists/getting-started-4.py
@@ -2,7 +2,7 @@ def events_counter(input_list):
     even_numbers = 0
     for a in input_list:
         if not isinstance(a, int):
-            raise ValueError('%s is not an integer!'%(a))
+            raise TypeError('%s is not an integer!'%(a))
         if a%2 == 0:
             even_numbers += 1
     return even_numbers
