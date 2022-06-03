979a0d30b06766b27eb4de5e62873df85c70adeb
stefangrgv <stefan.grgv@gmail.com>
2022-06-03 14:43:16 +0300

    Anagrams (#4)
    * zad3
    
    * changed the data input and return type
    
    * changed return expression
    
    * changed input method

---
+++ 01_lists/getting-started-3.py
@@ -0,0 +1,9 @@
+def is_anagrams(a, b):
+    return sorted(a.lower()) == sorted(b.lower())
+
+if __name__ == "__main__":
+    a, b = input('Enter first word: '), input('Enter second word: ')
+    if is_anagrams(a, b):
+        print('ANAGRAMS')
+    else:
+        print('NOT ANAGRAMS')
