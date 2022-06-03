e7e187dfd123c9abe922b3e86297a344f03d9293
stefangrgv <stefan.grgv@gmail.com>
2022-06-03 14:45:35 +0300

    list_examples (#10)
    * list_examples
    
    * changed negative list indexing
    
    * Delete list_examples.py
    
    * uploaded properly
    
    * recursive flatten function

--- 01_lists/list_flatten.py
+++ 01_lists/list_flatten.py
@@ -5,12 +5,12 @@ Task: Fill replace the ??? and define the flatten function
 >>> a = [[1, 2], [3, 4], [5, 6]]
 
 1. Flatten the list with list comprehension
->>> [item for l in a for item in l]
+>>> [i for l in a for i in l] 
 [1, 2, 3, 4, 5, 6]
 
 2. Flatten the list with list comprehension
 >>> a = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
->>> [item for l in a for ll in l for item in ll]
+>>> [i for l in a for ll in l for i in ll]
 [1, 2, 3, 4, 5, 6, 7, 8]
 
 2. Flatten the list with the more general function `flatten`
@@ -22,11 +22,12 @@ Hint: use list comprehension + recursion
 """
 
 
-def flatten(xs):
-    pass
+def flatten(l):
+    elements = [el for el in l if not isinstance(el, list)] # elements
+    lists = [el for el in l if isinstance(el, list)] # lists
+    return flatten(elements + [i for ll in lists for i in ll]) if len(lists) else elements
 
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    a = [1, 2, [3, 4], [[5, 6], [7, 8]]]
+    print(flatten(a))
