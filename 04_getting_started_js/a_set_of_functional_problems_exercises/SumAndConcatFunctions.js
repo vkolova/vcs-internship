bc9ad13a27e0a24eee69e7f73e7be1660c6c2c3a
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-11 16:20:06 +0300

    Sum and concat functions are made

--- 04_getting_started_js/a_set_of_functional_problems_exercises/SumAndConcatFunctions.js
+++ 04_getting_started_js/a_set_of_functional_problems_exercises/SumAndConcatFunctions.js
@@ -1,33 +1,19 @@
 /* eslint-disable linebreak-style */
 
-/**
-
- * Adds two numbers together
-
- * @param {int} num1 First number
- * @param {int} num2 Second number
- * @return {int} The sum of the two numbers.
-
- */
-function sum(num1, num2) {
+const sum = (num1, num2) =>{
   if (typeof(num1) === 'number' && typeof(num2) === 'number') {
     return num1 + num2;
   }
   throw new TypeError('Something is wrong with the types.');
-}
+};
 
-/**
-   * Adds two strings together
-   * @param {string} text1 First text
-   * @param {string} text2 Second text
-   * @return {string} The concatenation of two strings.
-   */
-function concat(text1, text2) {
+
+const concat = (text1, text2) =>{
   if (typeof(text1) === 'string' && typeof(text2) === 'string') {
     return text1 + text2;
   }
   throw new TypeError('Something is wrong with the types.');
-}
+};
 
 console.log(sum(3, 4));
 console.log(concat('Hello ', 'World!'));
