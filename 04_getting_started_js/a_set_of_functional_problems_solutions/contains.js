0093504f24b1b109347d69f412e417ad764a4bad
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-11 16:43:39 +0300

    'contains' and 'containsAll' problems solved

---
+++ 04_getting_started_js/a_set_of_functional_problems_solutions/contains.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+const contains = function(element, arr) {
+  return arr.includes(element) ? true : false;
+};
+
+console.log(contains(1, [1, 2, 3]));
--- 04_getting_started_js/a_set_of_functional_problems_solutions/sum_and_concat.js
+++ 04_getting_started_js/a_set_of_functional_problems_solutions/sum_and_concat.js
@@ -1,14 +1,14 @@
 /* eslint-disable linebreak-style */
-const sum = (a, b) =>{
-    (typeof (a) === 'number' && typeof (b) === 'number') ?
+const sum = function(a, b) {
+  return (typeof (a) === 'number' && typeof (b) === 'number') ?
     a + b : new TypeError('Something is wrong with the types.');
 };
 
-sum(1, 2);
+console.log(sum(1, 2));
 
-const concat = (a, b) =>{
-    (typeof (a) === 'string' && typeof (b) === 'string') ?
+const concat = function(a, b) {
+  return (typeof (a) === 'string' && typeof (b) === 'string') ?
     a + b : new TypeError('Something is wrong with the types.');
 };
 
-concat('a', 'b');
+console.log(concat('a', 'b'));
